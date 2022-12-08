abc66e71fc532307e6b5d47c1d6cbc0da77405c1
Christie Renaud <mail@christierenaud.com>
2022-12-08 15:00:08 -0500

    Image Component added, Cart and Favorite icons added

--- Context.js
+++ Context.js
@@ -1,8 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Context = React.createContext()
 
 function ContextProvider({ children }) {
-  return <Context.Provider value="">{children}</Context.Provider>
+  const [allPhotos, setAllPhotos] = useState([])
+  const [cartItems, setCartItems] = useState([])
+  const url =
+    'https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json'
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setAllPhotos(data))
+  }, [])
+  function toggleFavorite(id) {
+    setAllPhotos((prevArray) =>
+      prevArray.map((img) => {
+        if (img.id === id) {
+          return { url: img.url, id: img.id, isFavorite: !img.isFavorite }
+        } else {
+          return img
+        }
+      }),
+    )
+  }
+
+  function addToCart(item) {
+    setCartItems((prevCart) => [...prevCart, item])
+  }
+
+  function removeFromCart(id) {
+    setCartItems((prevCart) => {
+      return prevCart.filter((cartItem) => cartItem.id !== id)
+    })
+  }
+
+  return (
+    <Context.Provider
+      value={{
+        allPhotos,
+        cartItems,
+        toggleFavorite,
+        addToCart,
+        removeFromCart,
+      }}
+    >
+      {children}
+    </Context.Provider>
+  )
 }
 export { Context, ContextProvider }
--- pages/Cart.js
+++ pages/Cart.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Context } from '../Context.js'
 
 function Cart() {
   return (
